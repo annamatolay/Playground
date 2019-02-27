882418debb7a9859e19c1f983d9b5b303867d469
palko <matolaypal@gmail.com>
2019-02-27 22:12:04 +0100

    ArrayList 0.9

--- src/main/java/com/palmatolay/collections/ArrayList.java
+++ src/main/java/com/palmatolay/collections/ArrayList.java
@@ -1,16 +1,15 @@
 package com.palmatolay.collections;
 
-
-// TODO: set(Object o, int i), clear(), remove(int i)
+@SuppressWarnings("WeakerAccess")
 public class ArrayList<Type> {
 
     private Object[] array;
     private int size;
 
     public ArrayList() {
-        this.size = 0;
-        this.array = new Object[size];
+        createEmptyArray();
     }
+
 
     public int size() { return this.size; }
 
@@ -23,12 +22,33 @@ public class ArrayList<Type> {
     @SuppressWarnings("unchecked")
     public Type get(int index) { return (Type) array[index];}
 
+    //public Type set() {}
+
+    public void set(Type newValue, int index) { array[index] = newValue; }
+
+
+    public void remove(int index) {}
+
+    public void clear() { createEmptyArray(); }
+
+    private void createEmptyArray() {
+        this.size = 0;
+        this.array = new Object[size];
+    }
+
     private void increaseSize() {
         if (this.size == 0) { this.array = new Object[1]; }
         else if (this.size == this.array.length) {
-            Object[] tmpArray = this.array;
-            this.array = new Object[this.size * 2];
-            System.arraycopy(tmpArray, 0, this.array, 0, tmpArray.length);
+            copyArray(true);
         }
     }
+
+    @SuppressWarnings("SameParameterValue")
+    private void copyArray(boolean increaseSize) {
+        Object[] tmpArray = this.array;
+        if (increaseSize) this.array = new Object[this.size * 2];
+        else this.array = new Object[this.size];
+        System.arraycopy(tmpArray, 0, this.array, 0, tmpArray.length);
+    }
+
 }
--- src/test/java/com/palmatolay/collections/ArrayListTest.java
+++ src/test/java/com/palmatolay/collections/ArrayListTest.java
@@ -3,41 +3,59 @@ package com.palmatolay.collections;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.*;
+
+@SuppressWarnings("unchecked")
 public class ArrayListTest {
 
     private ArrayList list;
+    private final Integer DEFAULT_VALUE = 0;
+    private final List<Integer> EXPECTED_LIST = Arrays.asList(3, 6, 9);
 
     @Before
-    public void setUp() { list = new ArrayList<Integer>(); }
+    public void setUp() {
+        list = new ArrayList<Integer>();
+        list.add(DEFAULT_VALUE);
+    }
 
     @Test
     public void checkInstance() { assertNotNull(list); }
 
-    @Test
-    public void getSize() { assertEquals(0, list.size()); }
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void clear() { list.clear(); assertEquals(0, list.size()); list.get(DEFAULT_VALUE); }
 
     @Test
-    public void getSize_afterAddNumber() {
-        list.add(0);
-        assertEquals(1, list.size());
+    public void getSize() { assertEquals(1, list.size()); }
+
+    @Test
+    public void getDefaultElement() { assertEquals(DEFAULT_VALUE, list.get(DEFAULT_VALUE)); }
+
+    @Test
+    public void getSize_afterAddIntegers() {
+        addExpectedIntegers(false);
+        assertEquals(4, list.size());
     }
 
     @Test
-    public void addAndGetFirstElement() {
-        list.add(3);
-        assertEquals(3, list.get(0));
+    public void getAllElements_afterAddIntegers() {
+        addExpectedIntegers(true);
+        for (int i = DEFAULT_VALUE; i < list.size(); i++)
+            assertEquals(EXPECTED_LIST.get(i), list.get(i));
     }
 
     @Test
-    public void addAndGetMoreElements() {
-        int[] expected = {3, 6, 9};
-        for (int i : expected)
+    public void setAndGetNewInteger_atGivenIndex() {
+        int EXPECTED_VALUE = 1;
+        list.set(EXPECTED_VALUE, DEFAULT_VALUE);
+        assertEquals(EXPECTED_VALUE, list.get(DEFAULT_VALUE));
+    }
+
+    private void addExpectedIntegers(boolean createNewList) {
+        if (createNewList) list = new ArrayList<Integer>();
+        for (int i : EXPECTED_LIST)
             list.add(i);
-        assertEquals(3, list.size());
-        for (int i = 0; i < list.size(); i++)
-            assertEquals(expected[i], list.get(i));
     }
 }
