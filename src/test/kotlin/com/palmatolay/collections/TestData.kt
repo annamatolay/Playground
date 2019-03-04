d0fb8d88595cf8d3bb66ddcea9435cffd0d6fe66
palko <matolaypal@gmail.com>
2019-03-04 07:00:48 +0100

    TestData

---
+++ src/test/kotlin/com/palmatolay/collections/TestData.kt
@@ -0,0 +1,10 @@
+package com.palmatolay.collections
+
+class TestData{
+    companion object {
+        const val DEFAULT_INT = 0
+        const val DEFAULT_STRING = "DEFAULT"
+        const val DEFAULT_STRING_INT = "369"
+        val DEFAULT_LIST = listOf(3, 6, 9)
+    }
+}
